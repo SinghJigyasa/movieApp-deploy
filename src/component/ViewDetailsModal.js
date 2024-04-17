6b1090f57e57b4dc704f17017210a2c3fcd2181d
Jigyasa Singh <jig86026@gmail.com>
2024-04-17 14:41:37 +0530

    added animation

--- src/component/ViewDetailsModal.js
+++ src/component/ViewDetailsModal.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Modal, Table } from "react-bootstrap";
+import { Button, Card, Modal, Placeholder, Table } from "react-bootstrap";
 
 export const ViewDetailsModal = ({ show, onHide }) => {
   const [movieDetail, setMovieDetail] = useState([]);
@@ -19,58 +19,77 @@ export const ViewDetailsModal = ({ show, onHide }) => {
         console.log(err);
       });
   };
-
+  console.log(movieDetail.Response === "False", "length");
   return (
     <Modal show={show.show} onHide={onHide}>
       <Modal.Header closeButton>
         <Modal.Title className="fw-bold text-danger text-center">
-        <i className="bi bi-film me-2"></i>
+          <i className="bi bi-film me-2"></i>
           Movie Details
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <div class="row gy-4">
-          <div class="col-12">
-            <div className="img_area text-center">
-              <img
-                height={100}
-                src={movieDetail.Poster}
-                className="img-fluid rounded mx-auto"
-                alt={movieDetail.Poster}
-              />
-            </div>
-            <div className="content mt-4">
-              <h5 className="mb-4 fw-bold ">
-                {movieDetail.Title}({movieDetail.Year})
-              </h5>
-              <Table striped borderless size="sm">
-                <tbody>
-                  <tr>
-                    <td className="fw-bold">Genre:</td>
-                    <td>{movieDetail.Genre}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-bold">Cast:</td>
-                    <td>{movieDetail.Actors}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-bold">Duration:</td>
-                    <td>{movieDetail.Runtime}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-bold">Writer & Director:</td>
-                    <td>{movieDetail.Director}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-bold">Plot:</td>
-                    <td>{movieDetail.Plot}</td>
-                  </tr>
-                </tbody>
-              </Table>
+      {movieDetail.Response === "True" ? (
+        <Modal.Body>
+          <div class="row gy-4">
+            <div class="col-12">
+              <div className="img_area text-center">
+                <img
+                  height={100}
+                  src={movieDetail.Poster}
+                  className="img-fluid rounded mx-auto"
+                  alt={movieDetail.Poster}
+                />
+              </div>
+              <div className="content mt-4">
+                <h5 className="mb-4 fw-bold ">
+                  {movieDetail.Title}({movieDetail.Year})
+                </h5>
+                <Table striped borderless size="sm">
+                  <tbody>
+                    <tr>
+                      <td className="fw-bold">Genre:</td>
+                      <td>{movieDetail.Genre}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Cast:</td>
+                      <td>{movieDetail.Actors}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Duration:</td>
+                      <td>{movieDetail.Runtime}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Writer & Director:</td>
+                      <td>{movieDetail.Director}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Plot:</td>
+                      <td>{movieDetail.Plot}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
             </div>
           </div>
-        </div>
-      </Modal.Body>
+        </Modal.Body>
+      ) : (
+        <Modal.Body>
+          <>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={12} style={{ height: "10rem" }} />
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={12} />
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={12} />
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={12} />
+            </Placeholder>
+          </>
+        </Modal.Body>
+      )}
     </Modal>
   );
 };
